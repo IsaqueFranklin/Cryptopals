b18b12bcc33b92352c4dfb82c249aa203d951a9f
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-27 21:16:29 -0300

    fixedxor.go code complete

--- go/set1/fixedxor.go
+++ go/set1/fixedxor.go
@@ -3,10 +3,13 @@ package main
 import (
   "encoding/hex"
   "fmt"
+  "errors"
 )
 
 func main() {
-  
+ 
+  //Comentário futuro: o "_" usado como parâmetro duplo nas funções está sendo usado para ignorar o retorno de err que essas funções deveriam retornar. => Lembrar disso em outros projetos, conhecimento útil.
+
   input1, _ := decodeHex([]byte(""))
 
   input2, _ := decodeHex([]byte(""))
@@ -28,4 +31,16 @@ func decodeHex(input []byte) ([]byte, error) {
 
 func xorIt(input1, input2 []byte) ([]byte, error) {
 
+  //Checking if the inputs are equal
+  if input2 != input2 { 
+    return nil, errors.New("The inputs have different lengths, impossible to apply XOR.")
+  }
+
+  //Creating a buffer to hold the result value.
+  result := make([]byte, len(input1))
+
+  //Iterating trought the values of both inputs.
+  for i := 0; i < len(input2); i++ {
+    result[i] = input1[i] ^ input2[i]
+  }
 }
