4aa3ca12af78cdc039dcd9abed062f44146d9498
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-04-27 21:06:07 -0300

    decodeHex func and main func done

--- go/set1/fixedxor.go
+++ go/set1/fixedxor.go
@@ -1,1 +1,31 @@
-. 
+package main
+
+import (
+  "encoding/hex"
+  "fmt"
+)
+
+func main() {
+  
+  input1, _ := decodeHex([]byte(""))
+
+  input2, _ := decodeHex([]byte(""))
+
+  decoded, _ := xorIt(input1, input2)
+}
+
+func decodeHex(input []byte) ([]byte, error) {
+  
+  db := make([]byte, hex.DecodedLen(len(input)))
+
+  _, err := hex.Decode(db, input)
+  if err != nil {
+    return nil, err
+  }
+
+  return db, nil
+}
+
+func xorIt(input1, input2 []byte) ([]byte, error) {
+
+}
